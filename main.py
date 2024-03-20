1edecd0e0790b563bdea3059e946bb6d1755c28f
Mohammed Reda Belfaida <Mohammed-reda.belfaida@grenoble-inp.org>
2024-03-20 09:21:17 +0100

    modification réussie

--- main.py
+++ main.py
@@ -8,6 +8,29 @@ import sys
 from tycat import read_instance
 
 
+def is_inside(Point,polygone):
+    """
+    Implémentation du ray casting algorithm en python !
+    """
+    xp=Point.coordinates[0]
+    yp=Point.coordinates[1]
+    intersections=0
+    segments=polygone.segments()
+    for edge in segments:
+        first_point=edge.endpoints[0]
+        second_point=edge.endpoints[1]
+        x1=first_point.coordinates[0]
+        y1=first_point.coordinates[1]
+        x2=second_point.coordinates[0]
+        y2=second_point.coordinates[1]
+        try:
+            x0=x2+(x2-x1)*(yp-y2)/(y2-y1)
+            if (yp<y1)!=(yp<y2) and xp<x0:
+                intersections+=1
+        except ZeroDivisionError:
+            continue
+    return intersections%2==1
+
 def trouve_inclusions(polygones):
     """
     renvoie le vecteur des inclusions
@@ -15,7 +38,21 @@ def trouve_inclusions(polygones):
     contenant le ieme polygone (-1 si aucun).
     (voir le sujet pour plus d'info)
     """
-    return ["TODO"]
+    polygones_number = len(polygones)
+    inclusions_liste = [-1] * polygones_number
+
+    for i in range(polygones_number):
+        point_ref = polygones[i].points[0]  # Choisissez un point de référence dans le polygone i
+        reste_polygones = [(j,polygones[j]) for j in range(polygones_number) if j != i]
+
+        for j, poly in reste_polygones:
+            if is_inside(point_ref, poly):
+                inclusions_liste[i] = j
+                break  # Une fois qu'on a trouvé l'inclusion, on peut passer au polygone suivant
+            else:
+                continue
+
+    return inclusions_liste
 
 
 def main():
